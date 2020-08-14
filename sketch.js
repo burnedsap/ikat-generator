c50b0659d7dc4b9c196cd9f350e095531d2c3821
Salil <salil458@gmail.com>
2020-08-14 23:10:16 +0530

    added new patterns

--- sketch.js
+++ sketch.js
@@ -1,62 +1,57 @@
-var slider1, slider2, hslider, bslider, rsli, gsli, bsli;
-var w = 50;
-var y1 = 280;
-var hs, bs = 20;
-var rsl, gsl, bsl;
-//var w = slider.value;
-//	slider.oninput = function() {
-//	w = this.value;
-//	}
-//	
+var slider1, slider2, hslider, bslider, rsli, gsli, bsli, brsli, bgsli, bbsli;
+let sel, item;
 function setup() {
 	createCanvas(800, 600);
-	frameRate(5)
-	//	  noLoop()
-		  slider1 = createSlider(10, 70, 50);
+	frameRate(15)
+	sel = createSelect();
+	sel.option('one');
+	sel.option('two');
+	sel.option('three');
+		sel.selected('one');
+			sel.changed(mySelectEvent);
+	slider1 = createSlider(10, 150, 50);
 	slider2 = createSlider(100, 300, 280);
 	hslider = createSlider(1, 100, 20);
 	bslider = createSlider(10, 100, 20);
-	bsli = createSlider(0, 255, 20);
-	gsli = createSlider(0, 255, 20);
-	rsli = createSlider(0, 255, 20);
-}
-
-function updateSize() {
-	w = slider1.value()
-	y1 = slider2.value()
-	hs = hslider.value()
-	bs = bslider.value()
-	rsl = rsli.value()
-	bsl = bsli.value()
-	gsl = gsli.value()
+	bsli = createSlider(0, 255, 120);
+	gsli = createSlider(0, 255, 120);
+	rsli = createSlider(0, 255, 120);
+	brsli = createSlider(0, 255, 0);
+	bgsli = createSlider(0, 255, 0);
+	bbsli = createSlider(0, 255, 80);
 }
 
 function draw() {
 	background(0);
-	slider1.input(updateSize);
-	slider2.input(updateSize);
-	hslider.input(updateSize);
-	bslider.input(updateSize);
-	bsli.input(updateSize);
-	gsli.input(updateSize);
-	rsli.input(updateSize);
-	for (var x =  -200; x <width + 200; x += w) {
-		for (var y =  -200; y <height + 200; y += y1) {
-			One(x, y, color(rsl, gsl, bsl), hs, bs)
+	for (var x =  -200; x <width + 200; x += slider1.value()) {
+		for (var y =  -200; y <height + 200; y += slider2.value()) {
+			if (item == 'one') {
+				One(x, y, color(rsli.value(), gsli.value(), bsli.value()), hslider.value(), bslider.value())
+			} else if (item == 'two') {
+				Two(x, y);
+			}
+			else if (item == 'three') {
+				Three(x, y);
+			}
 		}
 	}
 	
-	for (n = 0; n < width; n += 1.5) {
-		stroke(10, 0, 100)
+	for (n = 0; n < width; n += 2.5) {
+		stroke(brsli.value(), bgsli.value(), bbsli.value())
 	    line(width, 0 + n, 0, 0 + n)
 	}
 }
 
+function mySelectEvent() {
+	item = sel.value();
+	//	return item;
+}
+
 function One(x, y, c, h, b) {
 	for (var a = 0; a < b; a += 1.3) {
-		r = random(0, h/2)
+		r = random(0, h / 2)
 	    stroke(c)
-	    line(x + a, y+r, x + a, y +h + r)
+	    line(x + a, y + r, x + a, y +h + r)
 	}
 }
 
