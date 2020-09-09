ed1e308f12498781efd7a1ebfc2240ada88ceb1a
burnedsap <salil458@gmail.com>
2020-09-09 20:01:02 +0530

    new controls

--- sketch.js
+++ sketch.js
@@ -1,18 +1,20 @@
+p5.disableFriendlyErrors = true; // disables FES
+
 var slider1, slider2, hslider, bslider, rsli, gsli, bsli, brsli, bgsli, bbsli;
 let sel;
 let item;
 var stag;
 let saveB;
 function setup() {
-	canvas = createCanvas(800, 800, P2D);
+	canvas = createCanvas(800, 800);
 	canvas.parent('sketch-holder');
     //	frameRate(15)
-//	labelC = createElement('p', 'Pattern Variables');
-//	labelC.addClass('labelC');
-//	labelC.style('margin', '0px');
-//	labelC.style('margin-top', '10px');
-//	labelC.style('margin-bottom', '3px');
-//	labelC.parent('sketch-holder');
+	labelC = createElement('p', 'Pattern Variables');
+	labelC.addClass('labelC');
+	labelC.style('margin', '0px');
+	labelC.style('margin-top', '10px');
+	labelC.style('margin-bottom', '3px');
+	labelC.parent('sketch-holder');
 	sel = createSelect();
 	sel.option('Select a pattern');
 	sel.option('Pattern One');
@@ -32,37 +34,76 @@ function setup() {
 	labelX.parent('sketch-holder');
 	slider2 = createSlider(50, 300, 180);
 	slider2.parent('sketch-holder');
+	brake = createElement('br');
+	brake.parent('sketch-holder');
+	labelX = createElement('label', 'Height');
+	labelX.parent('sketch-holder');
 	hslider = createSlider(1, 100, 20);
+	hslider.parent('sketch-holder');
+	labelX = createElement('label', 'Width');
+	labelX.parent('sketch-holder');
 	bslider = createSlider(10, 100, 20);
-	bsli = createSlider(0, 255, 120);
-	gsli = createSlider(0, 255, 120);
+	bslider.parent('sketch-holder');
+	labelC = createElement('p', 'Block Colour');
+	labelC.addClass('labelC');
+	labelC.style('margin', '0px');
+	labelC.style('margin-top', '10px');
+	labelC.style('margin-bottom', '3px');
+	labelC.parent('sketch-holder');
+	labelX = createElement('label', 'Red');
+	labelX.parent('sketch-holder');
 	rsli = createSlider(0, 255, 120);
+	rsli.parent('sketch-holder');
+	labelX = createElement('label', 'Green');
+	labelX.parent('sketch-holder');
+	gsli = createSlider(0, 255, 120);
+	gsli.parent('sketch-holder');
+	labelX = createElement('label', 'Blue');
+	labelX.parent('sketch-holder');
+	bsli = createSlider(0, 255, 120);
+	bsli.parent('sketch-holder');
+	labelC = createElement('p', 'Background Colour');
+	labelC.addClass('labelC');
+	labelC.style('margin', '0px');
+	labelC.style('margin-top', '10px');
+	labelC.style('margin-bottom', '3px');
+	labelC.parent('sketch-holder');
+	labelX = createElement('label', 'Red');
+	labelX.parent('sketch-holder');
 	brsli = createSlider(0, 255, 0);
+	brsli.parent('sketch-holder');
+	labelX = createElement('label', 'Green');
+	labelX.parent('sketch-holder');
 	bgsli = createSlider(0, 255, 0);
+	bgsli.parent('sketch-holder');
+	labelX = createElement('label', 'Blue');
+	labelX.parent('sketch-holder');
 	bbsli = createSlider(0, 255, 80);
+	bbsli.parent('sketch-holder');
+	brake = createElement('br');
+	brake.parent('sketch-holder');
 	saveB = createButton('Save Image');
 	saveB.mousePressed(saveImage);
+	saveB.parent('sketch-holder');
 }
 
 function draw() {
-	background(0);
+	background(252, 239, 224);
 	for (var x =  -200; x < width + 200; x += slider1.value()) {
 		for (var y =  -200; y < height + 200; y += slider2.value()) {
 			if (y % 2 == 0) {
 				stag = slider1.value() / 2;
-                //				console.log('50');
 			} else {
 				stag = 0;
-                //				console.log('0');
 			}
 			
-			if (item == 'one') {
+			if (item == 'Pattern One') {
 				One(x + stag, y, color(rsli.value(), gsli.value(), bsli.value()), hslider.value(), bslider.value())
-			} else if (item == 'two') {
+			} else if (item == 'Pattern Two') {
 				Two(x + stag, y, color(rsli.value(), gsli.value(), bsli.value()), hslider.value());
-			} else if (item == 'three') {
+			} else if (item == 'Pattern Three') {
 				Three(x + stag, y, color(rsli.value(), gsli.value(), bsli.value()), hslider.value());
-			} else if (item == 'four') {
+			} else if (item == 'Pattern Four') {
 				push();
 				translate(x + stag, y);
 				Four(x + stag, y, color(rsli.value(), gsli.value(), bsli.value()), hslider.value());
@@ -115,26 +156,11 @@ function Four(x, y, c, h) {
 	for (x = 0; x < 10; x += 1) {
 		stroke(c)
         r = random(0, h / 5)
-		
-		
-		        line(x + 10, 20 +y + r, x + 10, y + 54 + r + h) //long line
-		//        line(x + 20, 15+y + r, x + 20, y+20 + r)
-		//        line(x + 30, 12+y + r, x + 30, y+17 + r)
-		//        line(x + 40, 8+y + r, x + 40, 13+y + r)
-		        line(x + 50, 2 +y + r, x + 50, y + 68 + r + h) //long line
-		//        line(x + 60, 8+y + r, x + 60, 13+y + r)
-		//        line(x + 70, 11+y + r, x + 70, y+17 + r)
-		//        line(x + 80, 15+y + r, x + 80, y+20 + r)
-		
-		//        line(x + 10, 20+y + r, x + 10, y+54 + r)
-		//        line(x + 20, 59+y + r, x + 20, y+54 + r)
-		        line(x + 30, 64 +y + r + h, x + 30, y + 17 + r) //long line
-		//        line(x + 40, 70+y + r, x + 40, y+64 + r)
-		
-		//        line(x + 60, 64+y + r, x + 60, 70+y + r)
-		        line(x + 70, 16 +y + r, x + 70, y + 64 + r + h) //long line
-		//        line(x + 80, 54+y + r, x + 80+h, 60+y + r)
-		        line(x + 90, 20 +y + r, x + 90, y + 54 + r + h) //long line
+        line(x + 10, 20 +y + r, x + 10, y + 54 + r + h) //long line
+        line(x + 50, 2 +y + r, x + 50, y + 68 + r + h) //long line
+        line(x + 30, 64 +y + r + h, x + 30, y + 17 + r) //long line		
+        line(x + 70, 16 +y + r, x + 70, y + 64 + r + h) //long line
+        line(x + 90, 20 +y + r, x + 90, y + 54 + r + h) //long line
 	}
 }
 
